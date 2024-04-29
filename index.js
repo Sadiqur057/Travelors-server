a65991957db03225cb3081e349b4ac572379737d
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-29 22:47:41 +0600

    removed unnecessary comments

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-// Travelors-admin
-// eg6EB0Telg2KjFYx
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x7pm4nr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
